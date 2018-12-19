7e7a9e45652f4b6d8ef37c123ecd0f9068b63fe3
YiBiu <1551645539@qq.com>
2018-12-19 11:38:43 +0800

    modify basic lights

--- 2_2.BasicLights/object.fs
+++ 2_2.BasicLights/object.fs
@@ -1,10 +1,10 @@
 #version 330 core
 out vec4 FragColor;
   
-uniform vec3 objectColor;
-uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
 
 in vec3 Normal;
 in vec3 FragPos;
@@ -24,10 +24,10 @@ void main()
 	// specular
 	float specularStrength = 0.5;
 	vec3 viewDir = normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, norm);  
+	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
 	vec3 specular = specularStrength * spec * lightColor;
 	
 	vec3 result = (ambient + diffuse + specular) * objectColor;
-	FragColor = vec4(result, 1.0);
+    FragColor = vec4(result, 1.0);
 }
